f49ccde480a020b4a5c4423086e61d6ea86c651f
Helio_Anselmo <helioanselmo01@gmail.com>
2025-11-23 18:36:39 -0300

    refactor: Implementadas todas as funções principais do aplicativo:
              Recuperado e configurado o RadioButton para seleção da taxa de gorjeta.
    
              Implementado o cálculo total da gorjeta e divisão do valor entre as pessoas.
    
              Exibido o resultado final na tela para o usuário.
    
              Adicionada validação dos campos para evitar erros: caso o usuário não preencha todos os campos, é mostrado um aviso informando o preenchimento obrigatório.

--- app/src/main/java/com/example/appgorjeta/MainActivity.kt
+++ app/src/main/java/com/example/appgorjeta/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.appgorjeta
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appgorjeta.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,9 +22,55 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var percentage: Int = 0
+        binding.rbOptionOne.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                percentage = 10
+            }
+
+        }
+
+
+        binding.rbOptionTwo.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                percentage = 20
+            }
+        }
+
+
         binding.btnClean.setOnClickListener {
             println(" Helio 1" + binding.tieTotal.text)
             println(" Helio 1" + binding.tieNumberOfPeople.text)
+
+        }
+
+        binding.btnCalculate.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val nPeopleTemp = binding.tieNumberOfPeople.text
+
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true
+            ) {
+                Snackbar
+                    .make(binding.tieTotal, "Preencha todos os Campos", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val totalTable: Float = binding.tieTotal.text.toString().toFloat()
+                val nPeople: Int = binding.tieNumberOfPeople.text.toString().toInt()
+
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                binding.tvResult.text = "Valor dividido: $totalWithTips"
+            }
+
 
         }
 
